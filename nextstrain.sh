0d5c8a298677b320731699efdcff2bc2360800e0
Arnold Rodriguez-Hilario <54407020+arodzh-sudo@users.noreply.github.com>
2026-02-13 11:02:14 -0400

    udapted sbatch file

--- nextstrain.sh
+++ nextstrain.sh
@@ -3,7 +3,7 @@
 #SBATCH --qos=bphl-umbrella
 #SBATCH --job-name=nextstrain_orov
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=arnold.rodriguezhilario@flhealth.gov
+#SBATCH --mail-user=user@flhealth.gov
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=100gb
@@ -21,4 +21,5 @@ nextstrain build .
 cd ..
 
 # Import sequences from Juno output and run nextstrain's phylogenetic workflow
-python scripts/add_user_sequences.py --assembly-dir /blue/bphl-florida/arnold.rodriguez/repositories/bphl-molecular/Juno/output_reference/ --metadata my_samples.csv --rebuild
+# Change the path for the juno pipeline output
+python scripts/add_user_sequences.py --assembly-dir /path/to/Juno/output/ --metadata my_samples.csv --rebuild
